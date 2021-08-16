4ee717daecaf7ca587a3c88e6fd997ccd4267c44
clarelimo <clare.limo@student.moringaschool.com>
2021-08-16 12:12:46 +0300

    equals override

--- src/main/java/module/Animal.java
+++ src/main/java/module/Animal.java
@@ -1,14 +1,32 @@
 package module;
 
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class Animal extends Wildlife{
 
-    public Animal(String name, String health, String age){
+    public Animal(String name){
         this.name = name;
-        this.health = health;
-        this.age = age;
     }
 
+    @Override
+    public boolean equals(Object otherAnimal){
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName());
+        }
+    }
 
-
+    public static List<Animal> all() {
+        String sql = "SELECT * FROM animals WHERE type='animal';";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Animal.class);
+        }
+    }
 
 }
--- src/test/java/module/AnimalTest.java
+++ src/test/java/module/AnimalTest.java
@@ -9,10 +9,28 @@ public class AnimalTest {
     @Rule
     public DatabaseRule databaseRule = new DatabaseRule();
 
+    public Animal setUpAnimal(){
+        return new Animal("elephant");
+    }
+
     @Test
     public void animal_instantiatesCorrectly_true(){
-        Animal animal = new Animal("elephant","weak","young");
+//        Animal animal = new Animal("elephant","weak","young");
+        Animal animal = setUpAnimal();
         assertEquals(true,animal instanceof Animal);
+    }
+
+    @Test
+    public void animal_instantiatesWithName_String(){
+        Animal animal = setUpAnimal();
+        assertEquals("elephant",animal.getName());
+    }
+
+    @Test
+    public void equals_returnsTrueIfNameAreSame_true() {
+        Animal testAnimal = setUpAnimal();
+        Animal anotherAnimal = new Animal("elephant");
+        assertTrue(testAnimal.equals(anotherAnimal));
     }
 
 }
