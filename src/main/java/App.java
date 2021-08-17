28db80ae518ef57bb50f68f44ab88e1bd41d5e63
clarelimo <clare.limo@student.moringaschool.com>
2021-08-17 14:48:04 +0300

    index route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,6 @@ public class App {
             Map<String,Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
     }
 }
